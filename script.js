c4f57fe18ed19d0a0e49af3441156096d55fdf4d
DadVsGageGaming <gagergreer169@gmail.com>
2025-06-13 00:20:44 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function openFullscreen(button) {
+  const iframe = button.nextElementSibling;
+  if (iframe.requestFullscreen) iframe.requestFullscreen();
+  else if (iframe.mozRequestFullScreen) iframe.mozRequestFullScreen();
+  else if (iframe.webkitRequestFullscreen) iframe.webkitRequestFullscreen();
+  else if (iframe.msRequestFullscreen) iframe.msRequestFullscreen();
+}
